e8daed1ba0d8334c0d0d4488d6a67d62fca0eca0
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-11 16:06:58 +0100

    first method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/docking_station.rb'
-
+# expect(track_motivation(5)).to eq "Motivation stored!"
 describe DockingStation do
-  
+  it 'dokingstation should release bike' do
+    expect(subject).to respond_to :release_bike
+  end
+
 end
