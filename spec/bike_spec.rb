bf809737bfe2491453750b9c00eb574ed70f46ba
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-12 11:35:20 +0100

    tidy up

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,4 @@
 require_relative '../lib/bike.rb'
-
-# expect(track_motivation(5)).to eq "Motivation stored!"
 describe Bike do
   it 'bike should report working' do
     expect(subject).to respond_to :working?
