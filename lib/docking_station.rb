6f1a713dc536f8c85ef09c82fe6043d73e4e6c69
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-14 08:09:07 +0100

    guard statement created for bikes_unavailable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,25 @@
 require_relative '../lib/bike.rb'
 
 class DockingStation
+  BIKE_CAPACITY = 10
+
+  def initialize
+    @bikes_available = BIKE_CAPACITY
+  end
+
   def release_bike
-    Bike.new
+    if bikes_available > 0
+      Bike.new
+    else
+      bikes_unavailable
+    end
   end
 
   def bikes_unavailable
     raise 'No available bikes'
   end
-end 
+
+  def bikes_available
+    @bikes_available
+  end
+end
