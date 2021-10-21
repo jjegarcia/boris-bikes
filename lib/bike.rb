7df9c6e7959ec7b17a6eaa54d043a8bfa592c50a
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-21 18:42:05 +0100

    update

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,13 @@
 class Bike
+
   def working?
     true
+  end
+  def report_broken
+
+  end
+
+  def broken?
+    # code here
   end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,10 @@
 require_relative '../lib/bike.rb'
+subject = Bike.new
 describe Bike do
   it 'bike should report working' do
     expect(subject).to respond_to :working?
   end
+  it 'report broken should return broken as true'
+  subject.report_broken
+  expect(subject.broken?).to be_broken
 end
